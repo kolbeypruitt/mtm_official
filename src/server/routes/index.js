a7a7a6834bb86cede24a51d690ccbb761ae95f88
kolbeypruitt <kolbeypruitt@gmail.com>
2016-02-18 03:33:19 -0700

    added better products page with centered pictures

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'req.user'});
-});
-
-router.get('/ping', function(req, res, next) {
-  res.send("pong!");
+  res.render('index', {user: req.user});
 });
 
 router.get('/photos', function(req, res, next) {
-  res.render('photos', {title: "Mike's Photos"});
+  res.render('photos', {title: "Mike's Photos", user: req.user});
+});
+
+router.get('/followMe', function(req, res, next) {
+  res.render('followMe', {user: req.user});
 });
 
 module.exports = router;
